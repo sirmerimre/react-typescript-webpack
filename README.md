df7abb28a3b1f9e31cb45ec4361175ecdffce0e9
sirmerimre <sirmer.imre@gmail.com>
2021-06-27 17:37:53 +0100

    Adding @babel/runtime and transform-runtime to enable async await in code Adding copy webpack plugin to copy static assets in the bundle folder Optionally add bundleanalyzer to check the bundle sizes when built

---
+++ README.md
@@ -0,0 +1,10 @@
+Start the project
+
+``yarn; yarn start``
+
+Build a Prod bundle and test locally
+
+- ``yarn build``
+- ``cd build``
+- ``npx serve``   
+
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,12 +1,5 @@
-// const webpack = require('webpack');
 
 module.exports = {
-    mode: 'production',
-    devtool: 'source-map',
-    // To modify the env name
-    // plugins: [
-    //     new webpack.DefinePlugin({
-    //         'process.env.name': JSON.stringify('Codevolution'),
-    //     }),
-    // ],
-};
+  mode: 'production',
+  devtool: 'source-map',
+}
